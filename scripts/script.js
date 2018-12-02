21dd2c4a4f9df9a68ba513920167ee38138583c9
Manuele Onofri <manuele.onofri@gmail.com>
2018-12-03 00:54:33 +0100

    Modify navigation and add menu icon toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-console.log("Here I am!");
+let menu_icon = document.querySelector(".menu_icon");
+let navigation = document.querySelector("nav");
+menu_icon.addEventListener('click', () => {
+  navigation.classList.toggle("hide");
+});
