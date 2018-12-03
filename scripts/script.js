ae8b5def6191e07360ca6b39a3629067ded3e5d2
Manuele Onofri <manuele.onofri@gmail.com>
2018-12-04 00:28:51 +0100

    Add animated menu icon and improve style

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,5 @@ let menu_icon = document.querySelector(".menu_icon");
 let navigation = document.querySelector("nav");
 menu_icon.addEventListener('click', () => {
   navigation.classList.toggle("hide");
+  menu_icon.classList.toggle("change");
 });
