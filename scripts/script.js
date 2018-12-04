03fc31eb5ad65f73432942acf49ff7f41d82dad6
Manuele Onofri <manuele.onofri@gmail.com>
2018-12-04 01:31:23 +0100

    Add sticky navbar for small viewport

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
-let menu_icon = document.querySelector(".menu_icon");
-let navigation = document.querySelector("nav");
+const menu_icon = document.querySelector(".menu_icon");
+const navigation = document.querySelector("nav");
+const header = document.querySelector('#header');
+const body = document.querySelector("body");
+const sticky = header.offsetTop + 25;
+
 menu_icon.addEventListener('click', () => {
   navigation.classList.toggle("hide");
   menu_icon.classList.toggle("change");
+});
+
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+    body.classList.add("stack");
+  } else {
+    header.classList.remove("sticky");
+    body.classList.remove("stack");
+  }
 });
